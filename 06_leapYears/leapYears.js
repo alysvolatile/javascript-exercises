efa0ea2fe18cb2d3e23bfdc26c08f9d8c1d5aeb9
Alys <alice.g.cooper@gmail.com>
2022-04-19 15:18:24 -0400

    Write code for leapyears!

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+// first divisible by 400
+  if (year % 400 === 0) {
+      return true;
+  // NOT divisible by 100
+    } else if (year % 100 === 0) {
+      return false;
+ // then by 4
+    } else if (year % 4 === 0) {
+      return true;
+  // else not
+    } else {
+      return false;
+  }
 };
 
 // Do not edit below this line
