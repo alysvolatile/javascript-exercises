958415a2cae42b5e8485f26a19cfe778b408628d
Alys <alice.g.cooper@gmail.com>
2022-04-26 16:42:44 -0400

    Create palindrome funct

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
-};
+const palindromes = function (word) {
+ //remove punctation + spaces
+ let remove = /[^A-Za-z0-9]/g;
+ let noPunct = word.toLowerCase().replace(remove, "");
+ //create new var that is the word reversed
+ let newWord = noPunct.split("").reverse().join("");
+ //compare that to old word
+ return newWord === noPunct;
+ }
 
 // Do not edit below this line
 module.exports = palindromes;
