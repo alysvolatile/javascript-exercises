32a640f38770e3990cb95b14c90e0f8e2eae7d26
Alys <alice.g.cooper@gmail.com>
2022-04-26 16:43:16 -0400

    Create calc function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,32 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((partialSum, newValue) => partialSum + newValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((a,b) => a * b);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(num) {
+	// factorial needs to list all numbers between x and 1, else 1
+  if (num === 0 || num === 1)
+    return 1;
+  for (let i = num - 1; i >= 1; i--) {
+    num *= i;
+  }
+  return num;
+}
 
 // Do not edit below this line
 module.exports = {
