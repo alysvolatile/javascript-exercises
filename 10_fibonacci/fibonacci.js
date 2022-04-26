6eee0233616740bdf07dbcdf89f20f18e9188660
Alys <alice.g.cooper@gmail.com>
2022-04-26 18:50:50 -0400

    Create fibonacci seq

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+// num is the loop number
+let n = parseInt(num);
+if (n <= 0)
+  return "OOPS";
+let fib = [0,1]
+// make fibonacci loop
+for (let i = 2; i <= n; i++) {
+  fib[i] = fib[i - 1] + fib[i - 2];
+  fib.push(fib[i]);
+} 
+return fib = fib.pop();
+}
 
-};
-
+// use reduce function
+// return variable result
 // Do not edit below this line
 module.exports = fibonacci;
