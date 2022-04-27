2a9a25625006eebba82d0332a4cb8c54736fbb9f
Alys <alice.g.cooper@gmail.com>
2022-04-27 14:45:20 -0400

    Write getOldest code

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+// define current year   
+const currentYear = new Date().getFullYear();
 
+if (people.hasOwnProperty("yearOfDeath")) {
+  return;
+} else {
+    people.yearofDeath = `${currentYear}`;
+};
+
+const oldest = people.sort(function(a,b) {
+  const last = (a.yearOfDeath || currentYear) - a.yearOfBirth;
+  const next = (b.yearOfDeath || currentYear) - b.yearOfBirth;
+  return last > next ? 1 : -1;
+});
+
+// and if no yearOfDeath, use currentYear
+// define oldest person and return that at end - should be obj
+return oldest.pop();
 };
 
 // Do not edit below this line
